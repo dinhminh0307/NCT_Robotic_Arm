f6e87383025d5488c443e4b644e589ddc5c0ee05
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-10-18 11:56:53 +0700

    Update Pinchange interupt for each pin and enable the ADC

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ And the Arduino has 4 analog pin to handle the servos
  .Always enable EDAN bit for ADC conversion
  . Bit 6 – ADSC: ADC Start Conversion always write this bit to 1 in each conversion to start it
 */
+#include <Arduino.h>
 #include <avr/io.h>
 //------------Define Servo pins---------------//
 #define SERVO1 1
@@ -47,6 +48,37 @@ void PCINT_Enable() { // This function to receive input from analog pins
   // Enable pin change interupt regiter 0 bc it suitable fro PC[0:7]
   PCICR |= (1 << PCIE0);
 }
+
+//--------------Logical Functions ----------------------//
+void checkAnalogPin(int flags) {
+  switch (flags) {
+    case 0: // Enable ADC0
+      ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+      break;
+    case 1: // Enable ADC1
+      ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1));
+      ADMUX |= (1 << MUX0);
+      break;
+    case 2: // Enable ADC2
+      ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX0));
+      ADMUX |= (1 << MUX1);
+      break;
+    case 3: // Enable ADC3
+      ADMUX &= ~((1 << MUX3) | (1 << MUX2));
+      ADMUX |= ((1 << MUX0) | (1 << MUX1));
+      break;
+    case 4: // Enable ADC4
+      ADMUX &= ~((1 << MUX3) | (1 << MUX1) | (1 << MUX0));
+      ADMUX |= (1 << MUX2);
+      break;
+    case 5: // Enable ADC5
+      ADMUX &= ~((1 << MUX3) | (1 << MUX1));
+      ADMUX |= ((1 << MUX2) | (1 << MUX0));
+      break;
+    default:
+      break;
+  }
+}
 int main(void) {
   Arm_Config();
   ADC_Config();
@@ -56,8 +88,15 @@ int main(void) {
 }
 
 ISR(PCINT0_vect) { // Interupt when receive signal from analogpin
-  for(int i = 0; i <= 7;i++) {
-    
+  for(int i = 0; i <= 5;i++) {
+    if(PINC & (1 << i)) {
+      analFlag++;
+      checkAnalogPin(analFlag);
+    }
+    else {
+      continue;
+    }
   }
+  analFlag = 0;
 }
 
