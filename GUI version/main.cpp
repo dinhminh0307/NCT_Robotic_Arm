f191e0f565a891f6393338984de7b036c7daf6f8
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-10-23 19:12:25 +0700

    Delete filler

--- GUI version/main.cpp
+++ GUI version/main.cpp
@@ -1,17 +1,17 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+// #include <QGuiApplication>
+// #include <QQmlApplicationEngine>
 
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
+// int main(int argc, char *argv[])
+// {
+//     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/Properties-And-Handler/Main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
+//     QQmlApplicationEngine engine;
+//     const QUrl url(u"qrc:/Properties-And-Handler/Main.qml"_qs);
+//     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+//         &app, []() { QCoreApplication::exit(-1); },
+//         Qt::QueuedConnection);
+//     engine.load(url);
 
-    return app.exec();
-}
+//     return app.exec();
+// }
